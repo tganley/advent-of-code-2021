fad868723370b7de8cecc0ae7268a4868b7b4447
tganley <thomasjganley@gmail.com>
2021-12-06 21:59:16 -0700

    Completed day 2

---
+++ day2/day2.py
@@ -0,0 +1,22 @@
+import numpy as np
+horiz = 0
+depth = 0
+aim = 0
+
+f = open("day2_data.txt","r")
+actions = f.readlines()
+
+for i in range(np.size(actions)):
+    actions[i] = actions[i].split()
+
+    if(actions[i][0] == "forward"):
+        horiz += int(actions[i][1])
+        depth += aim * int(actions[i][1])
+    elif(actions[i][0] == "down"):
+        aim += int(actions[i][1])
+    elif(actions[i][0] == "up"):
+        aim -= int(actions[i][1])
+
+print("Depth: ", depth)
+print("Horizontal: ", horiz)
+print("Depth * Horizontal: ", depth*horiz)
