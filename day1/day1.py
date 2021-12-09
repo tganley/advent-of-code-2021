f05991b6c3a7c4129ad55573135a08cd16ebf6e3
tganley <thomasjganley@gmail.com>
2021-12-08 20:58:25 -0700

    Finished part 2 of day 1

--- day1/day1.py
+++ day1/day1.py
@@ -1,12 +1,11 @@
 import numpy as np
 
 count = 0
-f = open("day1_data.txt","rb")
+f = open("day1/day1_data.txt","r")
 lines = list(map(int,f.readlines()))
 print(np.size(lines))
-for i in range(np.size(lines)-1):
-    if lines[i] < lines[i+1]:
-        #print(lines[i] + "<" + lines[i+1])
+for i in range(np.size(lines)-3):
+    if lines[i] < lines[i+3]:
         count += 1
 
 print(count)
